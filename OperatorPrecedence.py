a00bd5a8f90856408f407bd36876412553518480
Melissa Cutts <admin@thecodingphoenix.com>
2024-09-04 01:54:16 +0000

    first exercise

---
+++ OperatorPrecedence.py
@@ -0,0 +1,26 @@
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
+
+print((5 + 4) * 10 / 2)
+# (9 * 10 / 2)
+# (90 / 2)
+#(45)
+
+print(((5 + 4) * 10) / 2)
+# ((9 * 10) / 2)
+# (90 / 2)
+# (45)
+
+print((5 + 4) * (10 / 2))
+# (9 * 5)
+# (45)
+
+print(5 + (4 * 10) / 2)
+# (5 + 40 / 2)
+# (5 + 20)
+# (25)
+
+print(5 + 4 * 10 // 2)
+# (5 + 40 // 2)
+# (5 + 20)
+# (25)
